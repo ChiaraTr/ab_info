ef4d03ef503edaba3db05c01875708c600135263
ChiaraTr <95907484+ChiaraTr@users.noreply.github.com>
2021-12-10 10:57:13 +0000

    .

--- src/lib/prova_types.py
+++ src/lib/prova_types.py
@@ -21,14 +21,16 @@ k=5
 s=5+1j
 print(s,k,s+k,type(s+k))
 
-
-
-
-
-
-
 print(math.sin(5))
 print(cmath.sin(s))
 
 var1='Hello World!'
 print(var1[4:])
+print('var1[0]',var1[0])
+
+name = 'Peter'
+my_string = 'Hello %s, how are you? %s' % (name,'Ok')
+print(my_string)
+
+rstring = r'Hello /t World!' #funziona!
+print(rstring)
