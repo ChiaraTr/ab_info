d100f3232e02d57426c6efd432484614087922fd
ChiaraTr <95907484+ChiaraTr@users.noreply.github.com>
2021-12-10 10:41:08 +0000

    src/lib/prova_types.py

---
+++ src/lib/prova_types.py
@@ -0,0 +1,35 @@
+import sys
+import math
+import cmath
+
+print(sys.maxsize)
+#print(sys.float_info)
+
+a=100.0/3
+print(a,type(a))
+
+b=100.0//3
+print(b,type(b))
+
+c=100.%3
+print(c,type(c))
+
+d=divmod(100.0,3)
+print(d,type(d))
+
+k=5
+s=5+1j
+print(s,k,s+k,type(s+k))
+
+
+
+
+
+
+
+print(math.sin(5))
+print(cmath.sin(s))
+
+var1='Hello World!'
+print(var1[4:])
+
