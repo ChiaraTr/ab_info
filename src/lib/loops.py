0d787266594f5aadbb65217f9589189f9856a204
ChiaraTr <95907484+ChiaraTr@users.noreply.github.com>
2021-12-17 10:53:19 +0000

    + lezione6 pt.2

---
+++ src/lib/loops.py
@@ -0,0 +1,27 @@
+n = 23
+running = True
+
+while running:
+  guess = int(input('Enter an integer:'))
+  if guess == n:
+    print('You guessed!')
+    break
+  elif guess < n:
+    print('Higher that that!')
+  else:
+    print('Lower than that!')
+else:
+  print('While loop is over')
+
+for i in range(n):
+  if i <= n/2:
+    print(' ',i/2)
+else: 
+  print('  Stop')
+ 
+a=iter(list(range(10)))
+for i in a:
+  print(next(a))
+
+b='Ciao'
+for el in b: print('  ', el)
